b549a8a0908a5cfe7aa3fd66c70a99d6e091ed44
Quill <itsquillyoutside@hotmail.com>
2016-06-01 12:37:25 +0000

    Fixed #1 and #2

--- se-chat-oneboxer.user.js
+++ se-chat-oneboxer.user.js
@@ -1,1 +1,37 @@
+// ==UserScript==
+// @name SE-Chat-Oneboxer
+// @description A link oneboxer for SE Chat
+// @version 0.0.1
+// @match *://chat.stackexchange.com/rooms/*
+// @match *://chat.stackoverflow.com/rooms/*
+// @match *://chat.meta.stackexchange.com/rooms/*
+// @author The-Quill
+// @downloadURL  https://github.com/The-Quill/SE-Chat-Oneboxer/raw/master/se-chat-oneboxer.user.js
+// @updateURL https://github.com/The-Quill/SE-Chat-Oneboxer/raw/master/se-chat-oneboxer.user.js
+// @run-at document-end
+// ==/UserScript==
 
+var formats = {
+    'videos': {
+        'on': false,
+        'link_match': /\.(avi|mov|mp4|webm)$/,
+        'format': '<video src="##link##" controls style="width: 300px; height: 150px">Sorry, your browser doesn\'t support the video tag.</video>'
+    }
+};
+var formatKeys = Object.keys(formats);
+
+var messages = document.querySelectorAll('.content');
+for (let i = 0; i < messages.length; i++){
+    var message = messages[i];
+    var links = message.querySelectorAll('a');
+    var text = 'http://' + message.textContent;
+    if (links.length > 0){
+        text = links[links.length - 1].href;
+    }
+    formatKeys.forEach(function(key){
+        var format = formats[key];
+        if (text.match(format.link_match) != null){
+            message.innerHTML = format.format.replace('##link##', text);
+        }
+    });
+}
