103e5693d92cb76a10d60172598088e2cbd52f94
Quill <itsquillyoutside@hotmail.com>
2016-06-01 13:41:21 +0000

    Fixed #4

--- se-chat-oneboxer.user.js
+++ se-chat-oneboxer.user.js
@@ -12,6 +12,13 @@
 // @run-at document-end
 // ==/UserScript==
 
+window.addEventListener("DOMNodeInserted", convert);
+var instagram = document.createElement('script');
+instagram.setAttribute('async', '');
+instagram.setAttribute('defer', '');
+instagram.src = '//platform.instagram.com/en_US/embeds.js';
+document.head.appendChild(instagram);
+
 var formats = {
     'videos': {
         'on': false,
@@ -25,39 +32,44 @@ var formats = {
     }
 };
 var formatKeys = Object.keys(formats);
-
-var messages = document.querySelectorAll('.content');
-for (let i = 0; i < messages.length; i++){
-    var message = messages[i];
-    var links = message.querySelectorAll('a');
-    var text = 'http://' + message.textContent;
-    if (links.length > 0){
-        text = links[links.length - 1].href;
-    }
-    formatKeys.forEach(function(key){
-        var format = formats[key];
-        if (format.format){
-            if (text.match(format.link_match) != null){
+function convert(){
+    var messages = document.querySelectorAll('.content');
+    for (let i = 0; i < messages.length; i++){
+        var message = messages[i];
+        if (message.classList.contains('processed')) continue;
+        message.classList.add('processed');
+        var links = message.querySelectorAll('a');
+        var text = 'http://' + message.textContent;
+        if (links.length > 0){
+            text = links[links.length - 1].href;
+        }
+        formatKeys.forEach(function(key){
+            var format = formats[key];
+            if (text.match(format.link_match) == null) return;
+            console.log(text);
+            if (format.hasOwnProperty('format')){
                 message.innerHTML = format.format.replace('##link##', text);
-            }
-        } else if (format.api){
-            if (text.match(format.link_match) != null){
+            } else if (format.hasOwnProperty('api')){
                 format.api(text, message);
             }
-        }
-    });
+        });
+    }
 }
 
 function instagram_api(link, element){
     GM_xmlhttpRequest({
         method: "GET",
-        url: "https://api.instagram.com/oembed",
-        data: "url="+link,
+        url: "https://api.instagram.com/oembed?url=" + link,
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
         },
         onload: function(response) {
-            element.innerHTML = response;
+            if (response.status != 200) return;
+            try {
+                element.innerHTML = "<div style='height: 403px; width: 300px;'>" + JSON.parse(response.responseText).html + "</div>";
+            }
+            catch (c){ return; }
+            if (window.instgrm) window.instgrm.Embeds.process();
         }
     });
 }
